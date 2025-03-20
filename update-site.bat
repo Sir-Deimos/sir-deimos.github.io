76716f3ef174b7a74b4afd948eb31696fea64ff4
Brett <brett.loyd@gmail.com>
2025-03-19 20:45:11 -0500

    Test header

--- update-site.bat
+++ update-site.bat
@@ -1,7 +1,20 @@
 @echo off
 
 :loop
-git add classresults.html rawresults.html
+
+set "source=C:\Program Files (x86)\Axware Systems\AXWare TS\announcer\results_live.html"
+set "dest=C:\users\hayde\documents\wkyautox\results_live.html"
+
+if exist "%source%" (
+    if exist "%dest%" (
+        fc /b "%source%" "%dest%" >nul
+        if errorlevel 1 copy /Y "%source%" "%dest%" >nul
+    ) else (
+        copy /Y "%source%" "%dest%" >nul
+    )
+)
+
+git add results_live.html
 git commit -m "Auto-update pages - %DATE% %TIME% || echo No changes to commit"
 git push
 timeout /t 60 /nobreak >nul
